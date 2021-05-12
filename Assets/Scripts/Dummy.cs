d99b9107501d420672eb4a4503520b02fbca2663
Michał Haracewiat <m.haracewiat@gmail.com>
2021-05-13 00:13:06 +0200

    chore: reorganize tests folder

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update");
+        transform.Translate(Vector3.up * Time.deltaTime * 10.0f);
+    }
+    
+    public int Add(int a, int b) {
+	    return a + b;   
+    }
+}
---
+++ Assets/Tests/PlayMode/DymmyPlayModeTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DymmyPlayModeTest
+    {
+        private Dummy dummy;
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator DymmyTestWithEnumeratorPasses()
+        {
+            GameObject dummyGameObject = new GameObject();
+            dummyGameObject.AddComponent<Dummy>();
+
+            float initialYPos = dummyGameObject.transform.position.y;
+
+            yield return new WaitForSeconds(.1f);
+
+            Assert.Less(initialYPos, dummyGameObject.transform.position.y);
+
+            yield return null;
+        }
+    }
+}
